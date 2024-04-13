2102d8e4979370ff1d898a0f476db8ba97b18a43
Woww2711 <120792827+Woww2711@users.noreply.github.com>
2024-04-13 18:14:57 +0700

    Files and things

---
+++ Data Collector/DataCollectorVideo.py
@@ -0,0 +1,156 @@
+import cv2
+from time import time
+import cvzone
+from cvzone.FaceDetectionModule import FaceDetector
+import os
+
+# This python file is for collecting data with existing videos
+
+####################################
+
+# REMINDER: CAREFUL WHEN LABELING IMAGES, ALWAYS CHECK THIS FIRST
+# IF YOU WANT TO ADD MORE CLASSES, REMEMBER TO COLLECT DATA RESPECTIVELY
+classID = 1
+# 0: Replay
+# 1: Real
+# 2: Mask
+
+# Create a temporary folder to hold all extracted frames for later use
+outputFolderPath = 'Your path here'
+confidence = 0.8
+
+# Change to False if you need to debug or examine
+save = True 
+
+# Larger is more focus
+blurThreshold = 30
+
+# For Debugging purpose
+debug = False
+
+# FaceDetection Module bounding boxes are a bit small, higher offset means bigger bboxes
+offsetPercentageW = 10
+offsetPercentageH = 20
+
+# This will affect your final images size
+camWidth, camHeight = 640, 480
+
+# For YOLO data format
+floatingPoint = 6
+
+# Directory to folder that contains videos
+dir = 'Your path here'
+vidList = list()
+
+for VidName in os.listdir(dir):
+    vidList.append(VidName)
+####################################
+
+for vid in vidList:
+    cap = cv2.VideoCapture(f'{dir}/{vid}')
+    cap.set(3, camWidth)
+    cap.set(4, camHeight)
+    ret = cap.grab()
+
+    vidFPS = cap.get(cv2.CAP_PROP_FPS) # Frames per second
+    frames = cap.get(cv2.CAP_PROP_FRAME_COUNT) # Total frames of video
+    duration = round(frames / vidFPS) # Video length
+
+    frame_no = 0
+    # Higher skip_rate means lesser frame to extract
+    # if duration >= 5:
+    #     skip_rate = duration * 4
+    # else:
+    #     skip_rate = 1
+    skip_rate = duration * 2
+
+    detector = FaceDetector()
+    while ret:
+        success, img = cap.read()
+
+        if not success:
+            break
+
+        frame_no += 1
+
+        if (frame_no % skip_rate == 0):
+            imgOut = img.copy()
+            img, bboxs = detector.findFaces(img, draw=False)
+
+            listBlur = []  # True False values indicating if the faces are blur or not
+            listInfo = []  # The normalized values and the class name for the label txt file
+            if bboxs:
+                # bboxInfo - "id","bbox","score","center"
+                for bbox in bboxs:
+                    x, y, w, h = bbox["bbox"]
+                    score = bbox["score"][0]
+                    # print(x, y, w, h)
+
+                    # ------  Check the score --------
+                    if score > confidence:
+
+                        # ------  Adding an offset to the face Detected --------
+                        offsetW = (offsetPercentageW / 100) * w
+                        x = int(x - offsetW)
+                        w = int(w + offsetW * 2)
+                        offsetH = (offsetPercentageH / 100) * h
+                        y = int(y - offsetH * 3)
+                        h = int(h + offsetH * 3.5)
+
+                        # ------  To avoid values below 0 --------
+                        if x < 0: x = 0
+                        if y < 0: y = 0
+                        if w < 0: w = 0
+                        if h < 0: h = 0
+
+                        # ------  Find Blurriness --------
+                        imgFace = img[y:y + h, x:x + w] 
+                        blurValue = int(cv2.Laplacian(imgFace, cv2.CV_64F).var())
+                        if blurValue > blurThreshold:
+                            listBlur.append(True)
+                        else:
+                            listBlur.append(False)
+
+                        # ------  Normalize Values  --------
+                        ih, iw, _ = img.shape
+                        xc, yc = x + w / 2, y + h / 2
+
+                        xcn, ycn = round(xc / iw, floatingPoint), round(yc / ih, floatingPoint)
+                        wn, hn = round(w / iw, floatingPoint), round(h / ih, floatingPoint)
+                        # print(xcn, ycn, wn, hn)
+
+                        # ------  To avoid values above 1 --------
+                        if xcn > 1: xcn = 1
+                        if ycn > 1: ycn = 1
+                        if wn > 1: wn = 1
+                        if hn > 1: hn = 1
+
+                        listInfo.append(f"{classID} {xcn} {ycn} {wn} {hn}\n")
+
+                        # ------  Drawing --------
+                        cv2.rectangle(imgOut, (x, y, w, h), (255, 0, 0), 3)
+                        cvzone.putTextRect(imgOut, f'Score: {int(score * 100)}% Blur: {blurValue}', (x, y - 0),
+                                        scale=2, thickness=3)
+                        if debug:
+                            cv2.rectangle(img, (x, y, w, h), (255, 0, 0), 3)
+                            cvzone.putTextRect(img, f'Score: {int(score * 100)}% Blur: {blurValue}', (x, y - 0),
+                                            scale=2, thickness=3)
+
+                # ------  To Save --------
+                if save:
+                    if all(listBlur) and listBlur != []:
+                        # ------  Save Image  --------
+                        timeNow = time()
+                        timeNow = str(timeNow).split('.')
+                        timeNow = timeNow[0] + timeNow[1]
+                        status, frame = cap.retrieve()
+                        cv2.imwrite(f"{outputFolderPath}/{timeNow}.jpg", frame)
+                        # ------  Save Label Text File  --------
+                        for info in listInfo:
+                            f = open(f"{outputFolderPath}/{timeNow}.txt", 'a')
+                            f.write(info)
+                            f.close()
+
+    # Un-comment for debug
+    # cv2.imshow("Image", imgOut)
+    # cv2.waitKey(1)
---
+++ Spliter.py
@@ -0,0 +1,88 @@
+import os
+import random
+import shutil
+from itertools import islice
+
+# This file is used when you feel like you have collected enough images for training
+# The script below split your data into 3 parts: train, val and test
+# It will also create a data.yaml file
+# Remember to compress 3 folders and 1 .yaml file to 1 .zip folder
+
+outputFolderPath = "Folder that all images are splited to train,val,test"
+inputFolderPath = "Folder that contains all images you want the model to train on"
+
+# Change these as per your need
+splitRatio = {"train": 0.8, "val": 0.1, "test": 0.1}
+classes = ["replay","real","mask"]
+
+try:
+    shutil.rmtree(outputFolderPath)
+except OSError as e:
+    os.mkdir(outputFolderPath)
+
+# --------  Directories to Create -----------
+os.makedirs(f"{outputFolderPath}/train/images", exist_ok=True)
+os.makedirs(f"{outputFolderPath}/train/labels", exist_ok=True)
+os.makedirs(f"{outputFolderPath}/val/images", exist_ok=True)
+os.makedirs(f"{outputFolderPath}/val/labels", exist_ok=True)
+os.makedirs(f"{outputFolderPath}/test/images", exist_ok=True)
+os.makedirs(f"{outputFolderPath}/test/labels", exist_ok=True)
+
+# --------  Get the Names  -----------
+listNames = os.listdir(inputFolderPath)
+
+uniqueNames = []
+for name in listNames:
+    uniqueNames.append(name.split('.')[0])
+uniqueNames = list(set(uniqueNames))
+
+# --------  Shuffle -----------
+random.shuffle(uniqueNames)
+
+# --------  Find the number of images for each folder -----------
+lenData = len(uniqueNames)
+lenTrain = int(lenData * splitRatio['train'])
+lenVal = int(lenData * splitRatio['val'])
+lenTest = int(lenData * splitRatio['test'])
+
+# --------  Put remaining images in Training -----------
+if lenData != lenTrain + lenTest + lenVal:
+    remaining = lenData - (lenTrain + lenTest + lenVal)
+    lenTrain += remaining
+
+# --------  Split the list -----------
+lengthToSplit = [lenTrain, lenVal, lenTest]
+Input = iter(uniqueNames)
+Output = [list(islice(Input, elem)) for elem in lengthToSplit]
+print(f'Total Images:{lenData} \nSplit: {len(Output[0])} {len(Output[1])} {len(Output[2])}')
+
+# --------  Copy the files  -----------
+
+sequence = ['train', 'val', 'test']
+for i,out in enumerate(Output):
+    for fileName in out:
+        shutil.copy(f'{inputFolderPath}/{fileName}.jpg', f'{outputFolderPath}/{sequence[i]}/images/{fileName}.jpg')
+        shutil.copy(f'{inputFolderPath}/{fileName}.txt', f'{outputFolderPath}/{sequence[i]}/labels/{fileName}.txt')
+
+print("Split Process Completed...")
+
+
+# -------- Creating Data.yaml file  -----------
+
+# REMINDER: BE VERY CAREFUL WHEN CREATING DATA.YAML FILE
+# TO TRAIN LOCALLY, YOU NEED TO SPECIFY THE ABSOLUTE PATH
+# CHECK THE DATA.YAML PATH THOROUGHLY BEFORE TRAINING, OR IT WILL FAIL AFTER FINAL EPOCH
+dataYaml = f'path: ../data\n\
+train: ../train/images\n\
+val: ../val/images\n\
+test: ../test/images\n\
+\n\
+nc: {len(classes)}\n\
+names: {classes}'
+
+
+f = open(f"{outputFolderPath}/data.yaml", 'a')
+f.write(dataYaml)
+f.close()
+
+print("Data.yaml file Created...")
